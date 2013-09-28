682cbe70d9b7546b3d90c96ecbfed10ad8f31090
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@8dac28c4-20c2-9f6b-1e76-5ad9d60e8322>
2013-09-28 22:34:58 +0000

    Ahora el Mover_B.sh comprueba si hay duplicados y actúa al respecto. Habría que revisar igual el cambio de nombre, lo hace bien, pero pone el número al final de la extensión (capaz lo quieren antes de la extensión, que se yo (?))

--- TP/temp/Mover_B.sh
+++ TP/temp/Mover_B.sh
@@ -3,7 +3,7 @@
 # Nombre: Mover_B
 #
 # Funcion para mover de archivos
-#
+# 
 
 # TODO: se podria hacer que devuelva distintos codigos de error para
 # identificarlos. Por ahora devuelve siempre 1 en caso de algun error.
@@ -14,7 +14,7 @@
 function existeOrigen {
 	if [ ! -f "$RUTA_ORIGEN" ] && [ ! -d "$RUTA_ORIGEN" ]; then
 		# archivo origen no existe / directorio no existe
-		echo "No existe el archivo origen!"
+		echo "No existe el archivo origen!" #DEBUG
 		exit 1
 	fi
 }
@@ -25,7 +25,7 @@ function existeOrigen {
 function existeDestino {
 	if [ ! -d "$RUTA_DESTINO" ]; then
 		# ruta destino no existe
-		echo "No existe el destino!"
+		echo "No existe el destino!" #DEBUG
 		exit 1
 	fi
 }
@@ -36,18 +36,53 @@ function existeDestino {
 function destinoEsOrigen {
 	if [ "$RUTA_DESTINO" == `dirname "$RUTA_ORIGEN"` ]; then
 		# se quiere mover al mismo lugar
-		echo "Mismo destino que origen!"
+		echo "Mismo destino que origen!" #DEBUG
 		exit 1
 	fi
 }
 
 # Verifica si ya existe un archivo en el destino con el mismo nombre
-# que el archivo de origen. En ese caso la variable de entorno DUPLICADO
-# se setea en "TRUE", sino queda en "FALSE".
+# que el archivo de origen. En ese caso crea una carpeta dup (si no existe)
+# y mueve el archivo a esa ruta, cambiandole el nombre agregando un
+# numero de referencia.
 # Trabaja con variables de entorno.
-function esDuplicado {
+function verificarDuplicado {
+	# Reviso si el archivo ya se encontraba en el destino (duplicado):
 	DUPLICADO="FALSE"
-	#TODO
+	for ARCHIVO in `find "$RUTA_DESTINO" -maxdepth 1` ; do
+		if [ $ARCHIVO == "$RUTA_DESTINO/$ARCHIVO_ORIGEN" ] ; then
+			DUPLICADO="TRUE"
+			echo "Archivo duplicado!" #DEBUG
+			break
+		fi
+	done
+	
+	# Si no estaba duplicado salimos y dejamos todo como estaba:
+	if [ $DUPLICADO == "FALSE" ] ; then
+		return 0
+	fi
+	
+	# Como estaba duplicado, vemos si existe el directorio "/dup" en el destino:
+	DIRECTORIO=`find "$RUTA_DESTINO" -maxdepth 1 -type d -name 'dup'`
+	DUP="FALSE"
+	NUM=1
+	if [ "$DIRECTORIO" == "$RUTA_DESTINO/dup" ] ; then
+		DUP="TRUE"
+		echo "/dup encontrado!" #DEBUG
+		# Aumentamos el contador:
+		for DUPLICADO in `find "$RUTA_DESTINO/dup" -maxdepth 1 -name "$ARCHIVO_ORIGEN.*"` ; do
+			let NUM=$NUM+1
+		done
+		ARCHIVO_DESTINO="$ARCHIVO_ORIGEN.$NUM" # Renombramos el archivo
+	fi
+	
+	# Si la carpeta dup no existe, es el primer duplicado:
+	if [ $DUP == "FALSE" ] ; then
+		mkdir "$RUTA_DESTINO/dup"
+		ARCHIVO_DESTINO="$ARCHIVO_ORIGEN.1"
+	fi
+	RUTA_DESTINO="$RUTA_DESTINO/dup"
+	echo "Se va a mover el archivo a $RUTA_DESTINO como $ARCHIVO_DESTINO" #DEBUG
 }
 
 # Funcion principal del script.
@@ -76,13 +111,9 @@ echo "Voy a mover el archivo $RUTA_ORIGEN a $RUTA_DESTINO"
 existeOrigen
 existeDestino
 destinoEsOrigen
-esDuplicado
-#if [ $DUPLICADO == "TRUE" ]; then
-#	generarDuplicado  # va a modificar RUTA_DESTINO y el ARCHIVO_DESTINO
-#	echo "El archivo estaba dupĺicado!"
-#fi
+verificarDuplicado # va a modificar RUTA_DESTINO y el ARCHIVO_DESTINO (en caso de ser necesario)
 
 # Movemos...
 mv "$RUTA_ORIGEN" "$RUTA_DESTINO/$ARCHIVO_DESTINO"
-echo "TODO BIEN!"
+echo "TODO BIEN!" #DEBUG
 exit 0
