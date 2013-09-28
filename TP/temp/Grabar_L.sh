610e1d207a77ffaaee7fd6cfc58513b242c42ef6
martine9111@gmail.com <martine9111@gmail.com@8dac28c4-20c2-9f6b-1e76-5ad9d60e8322>
2013-09-28 19:53:59 +0000

    Se agrega la verificacion del tamaño, y se trata especialmente el instalar, se agrega la fecha a los mensajes del log

--- TP/temp/Grabar_L.sh
+++ TP/temp/Grabar_L.sh
@@ -5,6 +5,25 @@
 # Escribe en el archivo 'comando'.$LOGEXT.
 
 
+function verificarRecorte(){
+	CANT_LINEAS=`cat $RUTA_LOG | wc -l`
+	if [ $CANT_LINEAS -gt $LOGSIZE ]
+	then
+		#Me quedo con las ultimas 50 lineas del log y agrego que recorto
+		TEMP_LOG=`echo "$RUTA_LOG".temp`
+		echo "Log Excedido, se recorta el log" > $TEMP_LOG
+		tail -n 50 $RUTA_LOG >> $TEMP_LOG
+		rm $RUTA_LOG
+		cat $TEMP_LOG > $RUTA_LOG
+		rm $TEMP_LOG
+	fi
+}
+
+function loggear_install(){
+	$RUTA_LOG=`echo "$CONFDIR"Instalar_B.log`
+	echo $MSJ_COMPLETO >> $RUTA_LOG 
+}
+
 #Verifico que no me hayan pasado una mala cantidad de parametros
 if [ $# -lt 1  -o $# -gt 3 ]
 then	
@@ -20,12 +39,20 @@ if [ $# == 3 ]
 then
 	TIPO_MSJ=$2
 	MENSAJE=$3
-	MSJ_COMPLETO=`echo ${TIPO_MSJ^^} - $MENSAJE`
+	MSJ_COMPLETO=`echo date ${TIPO_MSJ^^} - $MENSAJE`
 else	
 	TIPO_MSJ=""
 	MENSAJE=$2
-	MSJ_COMPLETO=`echo $MENSAJE`
+	MSJ_COMPLETO=`echo date $MENSAJE`
 fi
-RUTA_LOG=`echo $CALL_COMANDO.$LOGEXT`
 
-echo $MSJ_COMPLETO >> $RUTA_LOG
+if [ $CALL_COMANDO == "Instalar_B" ]
+then
+	loggear_install
+	exit 0
+fi
+
+RUTA_LOG=`echo $LOGDIR$CALL_COMANDO$LOGEXT`
+
+echo $MSJ_COMPLETO >> $RUTA_LOG 
+verificarRecorte
