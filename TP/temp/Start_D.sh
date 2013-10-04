0200cfd21eecd28ccfe9408b7796f3c5da17ecef
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@8dac28c4-20c2-9f6b-1e76-5ad9d60e8322>
2013-10-04 00:57:11 +0000

    typo

--- TP/temp/Start_D.sh
+++ TP/temp/Start_D.sh
@@ -8,5 +8,5 @@
 # Precondicion: Deben estar seteadas las variables de entorno e instalado todo.
 
 # TODO: en principio, solo el Recibir_B.sh
-nohup $BINDIR/Recibir_D.sh > /dev/null 2>&1 &
+nohup $BINDIR/Recibir_B.sh > /dev/null 2>&1 &
 exit 0
