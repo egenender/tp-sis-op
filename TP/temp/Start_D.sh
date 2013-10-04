f89cfb22bcd137d35fb2e414b248d22227989689
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@8dac28c4-20c2-9f6b-1e76-5ad9d60e8322>
2013-10-04 00:51:28 +0000

    Arreglada ruta de archivo de configuracion para el Iniciar_B.sh. Agregados Start_D y Stop_D, por el momento seteados para lanzar y detener el Recibir_B.sh

---
+++ TP/temp/Start_D.sh
@@ -0,0 +1,12 @@
+#!/bin/bash            
+#
+# Nombre: Start_D
+#
+# Inicia un demonio
+# 
+
+# Precondicion: Deben estar seteadas las variables de entorno e instalado todo.
+
+# TODO: en principio, solo el Recibir_B.sh
+nohup $BINDIR/Recibir_D.sh > /dev/null 2>&1 &
+exit 0
