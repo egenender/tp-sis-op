d6f48dcb51159e598fbe7699f9cf76581a8330fd
nicodiaz55.cp@gmail.com <nicodiaz55.cp@gmail.com@8dac28c4-20c2-9f6b-1e76-5ad9d60e8322>
2013-10-09 02:31:20 +0000

    Arreglado conflicto en Start y Stop

--- TP/temp/Stop_D.sh
+++ TP/temp/Stop_D.sh
@@ -9,9 +9,11 @@
 
 # Obtengo su PID:
 PID=`ps | grep "Recibir_B.sh" | head -1 | awk '{print $1 }'`
-if [ $PID == ""] ; then
+if [ -z "$PID" ]
+then
 	exit 1 # No habia proceso a detener
 else
 	kill -KILL $PID # Matamos el proceso
+	killall sleep 2> /dev/null
 	exit 0
 fi
