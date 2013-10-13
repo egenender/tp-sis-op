c2dfdbb7287ae085da1355782f84bc2c4c4e1d13
martine9111@gmail.com <martine9111@gmail.com@8dac28c4-20c2-9f6b-1e76-5ad9d60e8322>
2013-10-13 22:12:53 +0000

    Se modifica el recorte para que la cantidad de lineas a dejar del log original sea parametrizable

--- TP/temp/Grabar_L.sh
+++ TP/temp/Grabar_L.sh
@@ -2,8 +2,11 @@
 # Funcion para escribir un mensaje en el log, con origen y un tipo de mensaje (parametro opcional).
 # La funcion tiene la firma: Grabar_L comando_que_llama tipo_mensaje mensaje, donde tipo_mensaje es un
 # parametro opcional.
-# Escribe en el archivo 'comando'.$LOGEXT.
+# Escribe en el archivo 'comando'$LOGEXT.
 
+#Se define la constante de redimension, la cual puede ser modificada en cualquier
+#momento si es que así se desea
+CANT_LINEAS_REDIMENSION=50
 
 function verificarRecorte(){
 	CANT_LINEAS=`cat $RUTA_LOG | wc -l`
@@ -12,7 +15,7 @@ function verificarRecorte(){
 		#Me quedo con las ultimas 50 lineas del log y agrego que recorto
 		TEMP_LOG=`echo "$RUTA_LOG".temp`
 		echo "Log Excedido, se recorta el log" > $TEMP_LOG
-		tail -n 50 $RUTA_LOG >> $TEMP_LOG
+		tail -n $CANT_LINEAS_REDIMENSION $RUTA_LOG >> $TEMP_LOG
 		rm $RUTA_LOG
 		cat $TEMP_LOG > $RUTA_LOG
 		rm $TEMP_LOG
@@ -21,7 +24,7 @@ function verificarRecorte(){
 
 function loggear_install(){
 	
-	echo $MSJ_COMPLETO >> "$CONFDIR"Instalar_TP.log
+	echo $MSJ_COMPLETO >> "$CONFDIR"/Instalar_TP.log
 }
 
 #Verifico que no me hayan pasado una mala cantidad de parametros
@@ -52,7 +55,7 @@ then
 	exit 0
 fi
 
-RUTA_LOG=`echo $LOGDIR$CALL_COMANDO$LOGEXT`
+RUTA_LOG=`echo $LOGDIR/$CALL_COMANDO$LOGEXT`
 
 echo $MSJ_COMPLETO >> $RUTA_LOG 
 verificarRecorte
